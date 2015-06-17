a26eb822c63d695df78943216f761e419a89ed83
Markus Hauschild <hauschildm@gmx.net>
2015-06-17 20:13:55 +0200

    Use ldap/redis. Use external configuration file (example provided). Prepare login.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ TBA
 
 * py-flask >= 0.10
 * py-flask-wtf >= 0.10
+* py-ldap >= 2.4.15
+* py-redis >= 2.10
 
 ## Misc
 
--- index.py
+++ index.py
@@ -2,19 +2,36 @@
 
 from flask import Flask, render_template, redirect, url_for, session
 from flask_wtf import Form
+import ldap
+from redis import Redis
 from wtforms.fields import PasswordField, SelectField, StringField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'CHANGE!ME'
+app.config.from_pyfile('index.cfg')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+
+rdb = Redis(host='127.0.0.1', password='foobared')
+
+
+class LoginForm(Form):
+	user = StringField('Username', validators=[Required()])
+	pswd = PasswordField('Password', validators=[Required()])
+	submit = SubmitField('Login')
 
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		# TODO implement login with LDAP
+		return redirect(url_for('index'))
+	return render_template('login.html', form=form)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=True)
+	app.run(host='0.0.0.0', port=5000)
